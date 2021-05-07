9e5be4ac2935a734d27683ffd29aec13285fb710
ESMerrall <E.S.Merrall@liverpool.ac.uk>
2021-05-07 14:02:46 +0000

    Initial commit

--- R/hello.R
+++ R/hello.R
@@ -1,3 +1,25 @@
-hello_function(){
-  print("Hello, world!")
+#' Hello World!
+#'
+#'Print hello greeting
+#' @param name character string. Eve!
+#'
+#' @return prints hello greeting to console from me!
+#' @export
+#'
+#' @examples
+#' hello()
+#' hello("Lottielocks")
+hello <- function(name = NULL) {
+
+  # create greeting
+  if(is.null(name)){name <- "world"}
+  greeting <- paste("Hello", name, "from Eve!")
+
+  # randomly sample an animal
+  animal_names <- names(cowsay::animals)
+  i <- sample(1:length(animal_names), 1)
+
+  cowsay::say(greeting, animal_names[i])
 }
+
+# never use library function in a funtion. use double colon
